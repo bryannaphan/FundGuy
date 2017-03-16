c65d94da1c96e36445baa37f2a8ea8f99d1f97fb
danielledodd <danielledodd@umail.ucsb.edu>
2017-03-15 20:47:50 -0700

    added in nav bar and edited main activity

--- app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/MainActivity.java
@@ -64,12 +64,6 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -80,19 +74,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.budget) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.purchase_history) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.overview) {
 
-        } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
