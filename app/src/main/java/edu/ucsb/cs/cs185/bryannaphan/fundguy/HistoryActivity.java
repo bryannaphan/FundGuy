197e1261565de07043a6499569d0a0909252c0d8
johnny zhang <johnnyzhang@umail.ucsb.edu>
2017-03-16 21:32:01 -0700

    changed bar graph to 3.0 stuff

--- app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/HistoryActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/HistoryActivity.java
@@ -6,8 +6,8 @@ import android.view.View;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
-import edu.ucsb.cs.cs185.bryannaphan.fundguy.R;
-import edu.ucsb.cs.cs185.bryannaphan.fundguy.utilHistory.MPUtil;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
 
 import java.util.ArrayList;
@@ -36,14 +36,35 @@ public class HistoryActivity extends AppCompatActivity {
         dataList.add((float) 28);
         dataList.add((float) 3);
 
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 30f));
+        entries.add(new BarEntry(1f, 80f));
+        entries.add(new BarEntry(2f, 60f));
+        entries.add(new BarEntry(3f, 50f));
+        // gap of 2f
+        entries.add(new BarEntry(5f, 70f));
+        entries.add(new BarEntry(6f, 60f));
+
+        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+        BarData data = new BarData(set);
+        data.setBarWidth(0.9f); // set custom bar width
+        chart.setData(data);
+        chart.setFitBars(true); // make the x-axis fit exactly all bars
+        chart.invalidate(); // refresh
+
+
+
+
         //for(int i = 0; i<20; i++){
         //dataList.add((float) (r.nextInt(300-25)+25));
         //}
 
-        BarData barData2 = new BarData(MPUtil.getXAxisValues(20), MPUtil.getDataSet(HistoryActivity.this, dataList));
+        //BarData barData2 = new BarData(MPUtil.getXAxisValues(20), MPUtil.getDataSet(HistoryActivity.this, dataList));
 
-        MPUtil.getDataSet(HistoryActivity.this, dataList);
-        MPUtil.drawChart(HistoryActivity.this, chart, barData2);
+        //BarData barData2 = new BarData( dataList);
+
+        //MPUtil.getDataSet(HistoryActivity.this, dataList);
+       // MPUtil.drawChart(HistoryActivity.this, chart, barData2);
 
         chart.animateXY(3000, 3000);
     }
