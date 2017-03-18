f3abd536d87d073401ba9260fc4d303bd4560e5e
bryannaphan <bryannaphan@umail.ucsb.edu>
2017-03-18 12:52:35 -0700

    Revert "re-added pie chart categories connectivity"
    This reverts commit 5bafbe2dace7073ca416be6d2c464b7116a1564e.

--- app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/OverviewActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/OverviewActivity.java
@@ -1,7 +1,8 @@
 package edu.ucsb.cs.cs185.bryannaphan.fundguy;
-import android.icu.text.SelectFormat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -9,9 +10,6 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Created by JZ on 3/15/17.
@@ -22,18 +20,19 @@ public class OverviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Link elements from layout xml
         setContentView(R.layout.activity_overview);
 
         final PieChart pieChart = (PieChart) findViewById(R.id.chart);
 
-        ArrayList<String> categories = new ArrayList<>();
-        categories.add("Home & Utilities");
-        categories.add("Food");
-        categories.add("Transportation");
-        categories.add("Treat Yo Self");
-        categories.add("Other");
+        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
+        entries.add(new PieEntry(4f, "Fun"));
+        entries.add(new PieEntry(8f, "Bills"));
+        entries.add(new PieEntry(6f, "TreatYoSelf"));
+        entries.add(new PieEntry(12f, "Booze"));
+        entries.add(new PieEntry(18f, "Rent"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "# of calls");
+
 
         ArrayList<String> labels = new ArrayList<String>();
         labels.add("January");
@@ -43,43 +42,10 @@ public class OverviewActivity extends AppCompatActivity {
         labels.add("May");
         labels.add("June");
 
-        float sumHome=0; float sumFood=0; float sumTrans=0; float sumTreat=0; float sumOther=0;
-
-        // create a list of entries from the ListFragment
-        final List<PieEntry> entries = new ArrayList<>();
-        int entriesCount = ItemManager.getInstance().getSize();
-        for(int i = 0; i<entriesCount; i++) {
-            String entryCategory = ItemManager.getInstance().get(i).getCategory();
-            if (entryCategory == categories.get(0))
-                sumHome += ItemManager.getInstance().get(i).getAmount();
-            else if (entryCategory == categories.get(1))
-                sumFood += ItemManager.getInstance().get(i).getAmount();
-            else if (entryCategory == categories.get(2))
-                sumTrans += ItemManager.getInstance().get(i).getAmount();
-            else if (entryCategory == categories.get(3))
-                sumTreat += ItemManager.getInstance().get(i).getAmount();
-            else {
-                sumOther += ItemManager.getInstance().get(i).getAmount();
-            }
-        }
-
-        if (sumHome != 0)
-            entries.add(new PieEntry(sumHome,categories.get(0)));
-        if (sumFood != 0)
-            entries.add(new PieEntry(sumFood,categories.get(1)));
-        if (sumTrans != 0)
-            entries.add(new PieEntry(sumTrans,categories.get(2)));
-        if (sumTreat != 0)
-            entries.add(new PieEntry(sumTreat,categories.get(3)));
-        if (sumOther != 0)
-            entries.add(new PieEntry(sumOther,categories.get(4)));
-
-        PieDataSet dataSet = new PieDataSet(entries, "");
         PieData data = new PieData(dataSet);
-
-        // change the colors cause thats fun :D
-        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         pieChart.setData(data);
+
         pieChart.invalidate(); // refresh
     }
 }
