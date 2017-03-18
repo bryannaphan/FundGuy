0ab746adbf34133b769ff374ace3afabc237a868
bryannaphan <bryannaphan@umail.ucsb.edu>
2017-03-18 16:46:28 -0700

    item adapter connects to items in the list now

--- app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/MainActivity.java
@@ -14,10 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
     ItemAdapter itemAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +39,30 @@ public class MainActivity extends AppCompatActivity
                 fragment.show(fm, "Dialog Fragment");
             }
         });
+
         ListView lv = (ListView) findViewById(R.id.purchase_list);
         itemAdapter = new ItemAdapter(getBaseContext(), true);
         lv.setAdapter(itemAdapter);
 
+        // Get item clicked in list view
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int position, long id) {
+                Item clickedItem = (Item) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this,
+                        "Clicked item:\n" +
+                                clickedItem.getTitle() + ": " +
+                                clickedItem.getDescription(),
+                        Toast.LENGTH_SHORT).show();
+
+                Intent detailsIntent = new Intent(MainActivity.this, DetailsActivity.class);
+                detailsIntent.putExtra("position", position); // Pass in item position
+                MainActivity.this.startActivity(detailsIntent);
+            }}
+        );
+
+        // Navigation
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -60,7 +83,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -80,19 +102,24 @@ public class MainActivity extends AppCompatActivity
             Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
             //myIntent.putExtra("key", value); //Optional parameters
             MainActivity.this.startActivity(myIntent);
-        } else if (id == R.id.budget) {
+        }
 
-        } else if (id == R.id.purchase_history) {
+        else if (id == R.id.budget) {
+            // open budget fragment
+        }
+
+        else if (id == R.id.purchase_history) {
             Intent myIntent = new Intent(MainActivity.this, HistoryActivity.class);
             MainActivity.this.startActivity(myIntent);
-        } else if (id == R.id.overview) {
+        }
+
+        else if (id == R.id.overview) {
             Intent myIntent = new Intent(MainActivity.this, OverviewActivity.class);
             MainActivity.this.startActivity(myIntent);
-
         }
             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
             drawer.closeDrawer(GravityCompat.START);
             return true;
-
     }
+
 }
