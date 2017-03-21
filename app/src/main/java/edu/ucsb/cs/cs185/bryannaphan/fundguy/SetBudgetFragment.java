b3571845c817e4068b8a0b18248e4ea54c79f51e
johnny zhang <johnnyzhang@umail.ucsb.edu>
2017-03-21 02:59:19 -0700

    aesethic goalz

--- app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/SetBudgetFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs185/bryannaphan/fundguy/SetBudgetFragment.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs.cs185.bryannaphan.fundguy;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
